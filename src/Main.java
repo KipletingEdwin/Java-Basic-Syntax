4a9a5994d77b2dad11129ed39e9edb4e31a716b8
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-06-03 12:43:47 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the name to check...");
+        String name = scanner.nextLine();
+        System.out.println(name);
+
+
+//        for (int i = 0; i <= name.length(); i++) {
+//            System.out.println(i);
+//        }
+
+
+    }
+}
